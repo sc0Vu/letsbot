f8af60ee0b21782385c61def6b5b381146b0f239
sc0Vu <alk03073135@gmail.com>
2017-09-20 10:26:28 +0800

    Add date and directory existed check

--- ami/letsbot-nginx.sh
+++ ami/letsbot-nginx.sh
@@ -2,14 +2,21 @@
 # It's auto renew for certonly --standalone
 RENEW=false
 RENEWDAYS=7
+NOW=$(date +"%Y-%m-%d"+"%T")
+
+echo "[$NOW] letsbot started";
 
 for dir in /etc/letsencrypt/live/*/; do
-  for file in "$dir*cert.pem"; do
-    if ! openssl x509 -checkend $[ 86400 * $RENEWDAYS ] -noout -in $file; then
-      echo $file;
-      RENEW=true;
-    fi
-  done
+  if [ -d "$dir" ]; then
+    for file in "$dir*cert.pem"; do
+      if ! openssl x509 -checkend $[ 86400 * $RENEWDAYS ] -noout -in $file; then
+        echo "Outdated cert: $file";
+        RENEW=true;
+      fi
+    done
+  else 
+    echo "there is no live certificate in /etc/letsencrypt";
+  fi
 done
 
 if $RENEW; then
@@ -18,4 +25,6 @@ if $RENEW; then
   /usr/share/certbot-auto --debug --no-self-upgrade renew
   nginx;
   echo "success to renew certs";
+else
+  echo "nothing to renew";
 fi
